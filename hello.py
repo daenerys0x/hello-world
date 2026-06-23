577d426599e8cef29eef3d5333042d120d5ad56a
daenerys0x <gamerpiyush850@gmail.com>
2026-06-23 11:42:30 +0530

    Add hello world application

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Simple Hello World application."""
+
+
+def hello():
+    """Print Hello World message."""
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    print(hello())
